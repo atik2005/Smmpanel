8437d90dc0655553f804dd944d5a580b36715d63
atik2005 <mdforidfatema246810@gmail.com>
2025-04-21 19:55:37 +0600

    Create payment_methods.sql

---
+++ payment_methods.sql
@@ -0,0 +1,2 @@
+INSERT INTO `payment_methods` (`id`, `method_name`, `method_get`, `method_min`, `method_max`, `method_type`, `method_extras`, `method_line`) VALUES
+(49, 'Babypay', 'Babypay', 1, 10000, '2', '{\"method_type\":\"1\",\"name\":\"doniapay\",\"min\":\"1\",\"max\":\"10000\",\"api_key\":\"O84DUrXsrrZaTBbAWtJghnL\",\"secret_key\":\"42842362\",\"host_name\":\"babypay.site\",\"currency_rate\":\"100\"}', 1);
